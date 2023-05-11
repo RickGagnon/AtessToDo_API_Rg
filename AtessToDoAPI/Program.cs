551981b4c4ece4db798efa39ba51d5859e82af2f
RickGagnon <AtessRickGagnon@Outlook.com>
2023-05-11 16:53:01 -0400

    rtretr

--- AtessToDoAPI/Program.cs
+++ AtessToDoAPI/Program.cs
@@ -1,19 +1,25 @@
 using AtessToDoAPI;
+
 using AtessToDoAPI.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddDbContext<AtessTodoContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("AtessTodoContext")));
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => options.AddPolicy("TodoPolicy", policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddCors(options => options.AddPolicy("TodoPolicy", policy => policy.WithOrigins("http://localhost:4200","http://localhost:4200","http://localhost:7095","https://localhost:7095").AllowAnyHeader().AllowAnyMethod()));
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,10 +28,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("TodoPolicy");
 
 app.UseHttpsRedirection();
-app.UseCors("TodoPolicy");
 app.UseAuthorization();
+
 
 app.MapControllers();
 
